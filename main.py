98b98491936136a6abc57584dcf351e330f16fae
kh8832 <kazuki1884@icloud.com>
2021-09-14 13:51:38 +0900

    仕様4を実装

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-path = './dictionary-data.txt'
-f = open(path,'r',encoding='utf-8')
-words_in_file = f.readline()
+print('テキストファイルのパスを入力してください')
+path = input()
 
-print(words_in_file)
+f = open(path,'r',encoding='utf-8')
+words_in_file = f.readlines()
+f.close()
+
+for i,word in enumerate(words_in_file):
+    print(word,end='')
